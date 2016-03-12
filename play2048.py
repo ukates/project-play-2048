4e8f0d5ea6ec57419da235b2d471f24761b088d2
Hector Alfaro <halfaro1@valenciacollege.edu>
2016-03-12 00:56:50 -0500

    modify this file

---
+++ play2048.py
@@ -0,0 +1,10 @@
+#TODO: Import the module that will allow you to use Selenium
+#TODO: Import the module that will allow you to use the up, down, left, and right keys on your keyboard
+
+def play2048( times ):
+    #TODO: write code in this function that:
+    # 1. opens a game of 2048 from the URL: https://gabrielecirulli.github.io/2048/
+    # 2. uses the parameter 'times' to determine how many times your code will try to play the game
+    # 3. for each 'time', press these keys in this order: UP, RIGHT, DOWN, LEFT
+    # 4. print the final score after all tries to the screen 
+    
