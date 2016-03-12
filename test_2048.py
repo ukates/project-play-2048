1693311afa5a8f05463df0f081927694975a932d
Hector Alfaro <halfaro1@valenciacollege.edu>
2016-03-12 00:56:40 -0500

    do not edit this file

---
+++ test_2048.py
@@ -0,0 +1,13 @@
+import play2048
+
+def testGame5():
+    play2048.play2048(5)
+    assert True == True
+
+def testGame50():
+    play2048.play2048(50)
+    assert True == True
+
+def testGame100():
+    play2048.play2048(100)
+    assert True == True
